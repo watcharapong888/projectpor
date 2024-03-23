54de5aaa6b7b45303187c190374d1d5750e3fd0a
watcharapong888 <150762487+watcharapong888@users.noreply.github.com>
2024-03-24 00:51:07 +0700

    poop

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:projectpor.database.windows.net,1433; Database = projectpor", "adminbp", "P@ssword");
+    $conn = new PDO("sqlsrv:server = tcp:projectpor.database.windows.net,1433; Database = projectpor", "adminbp@projectpor", "P@ssword");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 }
