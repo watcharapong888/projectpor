5d47c64714c5817d054bf7ce0701d4b989f06f10
watcharapong888 <150762487+watcharapong888@users.noreply.github.com>
2024-03-24 00:45:57 +0700

    kkk

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
- 
+
 // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:projectpor.database.windows.net,1433; Database = projectpor", "adminbp", "P@ssword");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
 }
 catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
+    echo "Error connecting to SQL Server: " . $e->getMessage();
 }
 ?>
