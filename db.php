7b14b33057fbc216dfb47aa22e29f710c9ac3513
watcharapong888 <150762487+watcharapong888@users.noreply.github.com>
2024-05-01 14:48:05 +0700

    แก้db

--- db.php
+++ db.php
@@ -6,6 +6,7 @@
 
 try {
    $dateDB = "DATEDIFF(YEAR, date, GETDATE())" ;
+   $user = '"user"';
    $conn = new PDO("sqlsrv:server = tcp:projectpor.database.windows.net,1433; Database = projectpor", "adminbp@projectpor", "P@ssw0rd");
    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   //  echo "Connected successfully";
@@ -23,6 +24,7 @@ catch (PDOException $e) {
  
 // try {
 //   $dateDB = "TIMESTAMPDIFF(YEAR, date, CURDATE())"; 
+//   $user = 'user';
 //   $conn = new PDO("mysql:host=$servername;dbname=project;charset=utf8", $username, $password);
 //   // set the PDO error mode to exception
 //   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
