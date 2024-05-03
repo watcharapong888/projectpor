ecf3c9832283068ef70d37c836cffffa267ceafe
watcharapong888 <150762487+watcharapong888@users.noreply.github.com>
2024-05-03 23:18:52 +0700

    db nnnn

--- db.php
+++ db.php
@@ -4,17 +4,20 @@
 
 // //prod.DB <------------------------------------------------------------------------------------------------------------------------------->
 
-// PHP Data Objects(PDO) Sample Code:
+$servername = "projectmoo8.mysql.database.azure.com";
+$username = "napasorn"; //username
+$password = "P@ssw0rd"; //password
+ 
 try {
-    $conn = new PDO("sqlsrv:server = tcp:projectmmo8.database.windows.net,1433; Database = projectmoo8", "CloudSA3e4c16fa", "P@ssw0rd");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $dateDB = "TIMESTAMPDIFF(YEAR, date, CURDATE())"; 
+  $user = 'user';
+  $conn = new PDO("mysql:host=$servername;dbname=project;charset=utf8", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
 
 
 // //xampp.DB  <------------------------------------------------------------------------------------------------------------------------------->
