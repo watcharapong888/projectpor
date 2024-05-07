895e953c2fa692a0efa6fcc7bde5a537f8d5f5ef
watcharapong888 <150762487+watcharapong888@users.noreply.github.com>
2024-05-07 12:55:32 +0700

    แก้ login

--- db.php
+++ db.php
@@ -4,15 +4,19 @@
 
 // //prod.DB <------------------------------------------------------------------------------------------------------------------------------->
 
+$servername = "projectmoo8.mysql.database.azure.com";
+$username = "napasorn"; //username
+$password = "P@ssw0rd"; //password
+ 
 try {
-   $dateDB = "DATEDIFF(YEAR, date, GETDATE())" ;
-   $user = 'user';
-   $conn = new PDO("sqlsrv:server = tcp:projectpor.database.windows.net,1433; Database = projectpor", "adminbp@projectpor", "P@ssw0rd");
-   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //  echo "Connected successfully";
-}
-catch (PDOException $e) {
-   echo "Error connecting to SQL Server: " . $e->getMessage();
+  $dateDB = "TIMESTAMPDIFF(YEAR, date, CURDATE())"; 
+  $user = 'user';
+  $conn = new PDO("mysql:host=$servername;dbname=project;charset=utf8", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
 
 
